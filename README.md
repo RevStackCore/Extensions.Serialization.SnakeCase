f8e43d080bdc1c593d2590538eb06523f8487797
Scott <sfrancis@misinteractive.com>
2017-07-09 02:30:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#RevStackCore.Extensions.Serialization.SnakeCase
+
+
+
+
+
---
+++ RevStackCore.Extensions.Serialization.SnakeCase/JsonSnakeCase.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace RevStackCore.Extensions.Serialization.SnakeCase
+{
+    public static class Json
+	{
+		public static string SerializeObject<T>(T value)
+		{
+			DefaultContractResolver contractResolver = new DefaultContractResolver
+			{
+				NamingStrategy = new SnakeCaseNamingStrategy()
+			};
+
+			return JsonConvert.SerializeObject(value, new JsonSerializerSettings
+			{
+				ContractResolver = contractResolver,
+				Formatting = Formatting.Indented
+
+			});
+		}
+
+		public static T DeserializeObject<T>(string json)
+		{
+			DefaultContractResolver contractResolver = new DefaultContractResolver
+			{
+				NamingStrategy = new SnakeCaseNamingStrategy()
+			};
+
+			return JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings
+			{
+				ContractResolver = contractResolver,
+				Formatting = Formatting.Indented
+			});
+		}
+
+
+		public static T Cast<T>(object value)
+		{
+			var json = SerializeObject(value);
+			var obj = DeserializeObject<T>(json);
+			return obj;
+		}
+	}
+}
+
